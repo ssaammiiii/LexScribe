43e8fa429060457430be4ae5cb23efdba9addcce
Ishaan <ishaanbhardwaj1219@gmail.com>
2025-12-09 08:52:56 +0000

    Azure func

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from infrastructure import video_to_audio
+from intelligence import fetching_transcript
+import os
+
 def main():
-    print("Hello from lexscribe!")
+    video = "sample.mp4"
+    video_to_audio(video,"output.mp3")
+    target = "output.mp3"
+    if os.path.exists(target):
+        fetching_transcript(target)
+    else:
+        print("output.mp3 not found")
 
 
 if __name__ == "__main__":
